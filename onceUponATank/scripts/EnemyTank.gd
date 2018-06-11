b070a3809973f4fd27b78aecf01e005bd009ee84
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2018-06-11 22:28:52 +0200

    Improve enemy tank movement, add tank aiming

--- onceUponATank/scripts/EnemyTank.gd
+++ onceUponATank/scripts/EnemyTank.gd
@@ -2,10 +2,30 @@ extends "res://scripts/Tank.gd"
 
 onready var parent = get_parent()
 
+export (float) var turret_speed
+export (float) var detection_radius
+
+var target = null
+
 func control(delta):
 	# point to player
 	#$Turret.look_at(get_node("/root/Tank").position)
 	if parent is PathFollow2D:
 		parent.set_offset(parent.get_offset() + speed * delta)
+		position = Vector2()
 	else:
 		pass
+
+func _process(delta):
+	if target:
+		var target_dir = (target.global_position - global_position).normalized()
+		var current_dir = Vector2(1, 0).rotated($Turret.global_rotation)
+		$Turret.global_rotation = current_dir.linear_interpolate(target_dir, turret_speed * delta).angle()
+
+func _on_DetectionArea_body_entered(body):
+	if body.name == "Tank":
+		target = body
+
+func _on_DetectionArea_body_exited(body):
+	if body == target:
+		target = null
